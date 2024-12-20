5d44f285941ec3d4f679422a35b11a0701a41a81
raselworshop <raselworshop@gmail.com>
2024-12-21 04:37:41 +0800

    added the bid functionalities

--- src/pages/JobDetails.jsx
+++ src/pages/JobDetails.jsx
@@ -1,21 +1,25 @@
 import axios from 'axios';
-import { format } from 'date-fns';
-import { useCallback, useEffect, useState } from 'react'
+import { compareAsc, format } from 'date-fns';
+import { useCallback, useContext, useEffect, useState } from 'react'
 
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 import { useParams } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProvider';
+import Swal from 'sweetalert2';
 
 const JobDetails = () => {
+  const { user } = useContext(AuthContext)
   const [startDate, setStartDate] = useState(new Date());
   const [job, setJob] = useState({});
   const [error, setError] = useState(null);
   const { id } = useParams();
+
   const fetchJob = useCallback(async () => {
     try {
       const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/update/job/${id}`);
       setJob(data.result);
-      setStartDate(new Date(data.result.deadline))
+      // setStartDate(new Date(data.result.deadline))
       console.log(data.result)
     } catch (error) {
       console.error('Error fetching jobs:', error);
@@ -25,7 +29,7 @@ const JobDetails = () => {
   useEffect(() => {
     fetchJob();
   }, [fetchJob])
-  console.log(job)
+
   if (error) {
     return <p>{error}</p>
   }
@@ -37,7 +41,98 @@ const JobDetails = () => {
     max_price,
     description,
     buyer,
+    _id
   } = job || {};
+
+  // bids related functionalities
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    const form = e.target;
+    const price = form.price.value;
+    const comment = form.comment.value;
+    const email = user?.email;
+    const bidDeadline = startDate;
+    const bidId = _id;
+    const placedBid = { price, comment, email, bidDeadline, bidId }
+
+    // check bid validation permissions 
+    if (user?.email === buyer.email) {
+      return Swal.fire({
+        title: "Action not permitted!",
+        icon: "error",
+        draggable: true
+      });
+    }
+
+    // deadline validation for mini days to do the job
+    if (compareAsc(new Date(), new Date(deadline)) === 1) {
+      return Swal.fire({
+        title: "Deadline over, placing bid is forbidden!",
+        icon: "error",
+        draggable: true
+      });
+    }
+    // sellers deadline to complete the job after bid accepted by seller 
+    if (compareAsc(new Date(startDate), new Date(deadline)) === 1) {
+      return Swal.fire({
+        title: "Offer a deadline within provided date!",
+        icon: "error",
+        draggable: true
+      });
+    }
+    // default max price validation
+    if (price > max_price) {
+      return Swal.fire({
+        title: `You shoudn't bid over max-price: $${max_price}!`,
+        icon: "error",
+        draggable: true
+      });
+    }
+
+    try {
+      const { data } = await axios.post(`${import.meta.env.VITE_API_URL}/client/add-bid`, placedBid)
+      console.log(data)
+      form.reset();
+      if (data.insertedId) {
+        Swal.fire({
+          title: "Bid Grant Successfully!",
+          text: "You will be redirected to your placed bids.",
+          showClass: {
+            popup: `
+            animate__animated
+            animate__fadeInUp
+            animate__faster
+          `
+          },
+          hideClass: {
+            popup: `
+            animate__animated
+            animate__fadeOutDown
+            animate__faster
+          `
+          },
+          willClose: () => { window.location.href = '/my-bids'; }
+        });
+      }
+    } catch (error) {
+      console.error("Error bidding form data:", error);
+      Swal.fire({
+        title: "Error",
+        text: `${error.response.data.message || "There was an error submitting the bid. Please try again."}`,
+        icon: "error",
+        showClass: {
+          popup: ` animate__animated animate__shakeX animate__faster `
+        },
+        hideClass: {
+          popup: ` animate__animated animate__fadeOutDown animate__faster `
+        }
+      });
+    }
+
+    console.log(placedBid)
+    // console.table({placedBid})
+    // console.table(placedBid)
+  }
 
   return (
     <div className='flex flex-col md:flex-row justify-around gap-5  items-center min-h-[calc(100vh-306px)] md:max-w-screen-xl mx-auto '>
@@ -76,6 +171,7 @@ const JobDetails = () => {
             </div>
             <div className='rounded-full object-cover overflow-hidden w-14 h-14'>
               <img
+                referrerPolicy='no-referrer'
                 src={buyer?.photo}
                 alt=''
               />
@@ -92,7 +188,7 @@ const JobDetails = () => {
           Place A Bid
         </h2>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className='grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2'>
             <div>
               <label className='text-gray-700 ' htmlFor='price'>
@@ -115,6 +211,7 @@ const JobDetails = () => {
                 id='emailAddress'
                 type='email'
                 name='email'
+                defaultValue={user?.email}
                 disabled
                 className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md   focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring'
               />
